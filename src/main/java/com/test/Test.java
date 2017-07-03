ad2731e743e6cf843caeec1ad9b77755eeb72a56
Administrator <983576446@qq.com>
2017-07-03 15:10:52 +0800

    Initial commit

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created by Administrator on 2017/7/3.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first Git");
+    }
+}
